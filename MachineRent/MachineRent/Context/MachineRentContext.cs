b7a9177ca8493d3204735dbbf7b5194ffe87abeb
asiutra <adamsiutra@onet.pl>
2020-05-23 20:20:05 +0200

    Configuring project

---
+++ MachineRent/MachineRent/Context/MachineRentContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MachineRent.Context
+{
+    public class MachineRentContext : IdentityDbContext
+    {
+        public MachineRentContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- MachineRent/MachineRent/Startup.cs
+++ MachineRent/MachineRent/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MachineRent.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,18 @@ namespace MachineRent
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("appsettings.json").AddEnvironmentVariables("MACHINERENTAPP_");
+            Configuration = config.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +37,15 @@ namespace MachineRent
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
