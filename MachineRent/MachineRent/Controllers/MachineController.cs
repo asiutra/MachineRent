315facd3fbe165814f53d46a82f90fea64f0f155
asiutra <adamsiutra@onet.pl>
2020-06-06 17:35:17 +0200

    HandleReservation

--- MachineRent/MachineRent/Controllers/MachineController.cs
+++ MachineRent/MachineRent/Controllers/MachineController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MachineRent.Models;
 using MachineRent.Models.ViewModels;
 using MachineRent.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MachineRent.Controllers
@@ -11,10 +13,14 @@ namespace MachineRent.Controllers
     public class MachineController : Controller
     {
         private readonly IMachinesService _machinesService;
+        private readonly IReservationService _reservationService;
+        private UserManager<IdentityUser> _userManager;
 
-        public MachineController(IMachinesService machinesService)
+        public MachineController(IMachinesService machinesService, IReservationService reservationService, UserManager<IdentityUser> userManager)
         {
             this._machinesService = machinesService;
+            this._reservationService = reservationService;
+            this._userManager = userManager;
         }
 
         [HttpGet]
@@ -23,6 +29,40 @@ namespace MachineRent.Controllers
             var machines = await _machinesService.GetAllAsync();
 
             return View(machines);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FormReservation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FormReservation(Reservations model, int id)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Błąd dodawania rezerwacji.");
+                return View(model);
+            }
+
+            var reservation = new Reservations()
+            {
+                RentFromDate = model.RentFromDate,
+                RentToDate = model.RentToDate,
+                MachineId = id,
+                User = user
+            };
+
+            var result = await _reservationService.AddReservationAsync(reservation);
+            
+            //TODO: 
+            return RedirectToAction("Index", "Home");
         }
     }
 }
